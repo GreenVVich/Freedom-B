2709be8e074a8d583b53f4a40552bbc30ecabe4e
GreenVVich <96542151+GreenVVich@users.noreply.github.com>
2024-02-28 00:20:18 +0300

    DB engines update

--- database.py
+++ database.py
@@ -4,27 +4,29 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.ext.asyncio import create_async_engine
 from asyncio import run
 
-engine = create_engine(
-    url=settings.db_url,
-    echo=True)
+engine = create_engine(url=settings.db_url, echo=True)
 
-async_engine = create_async_engine(
-    url=settings.db_url_async,
-    echo=True)
+async_engine = create_async_engine(url=settings.db_url_async, echo=True)
 
-# with engine.connect() as con:
-#     query = text("SELECT * FROM public.text_collector")
-#     res = con.execute(query).all()
-#     # con.commit()
-#     print(f"{res=}")
+
+def sync_select():
+    print('Начало')
+    with engine.connect() as con:
+        query = text("SELECT * FROM public.text_collector")
+        res = con.execute(query).all()
+        print(f"{res=}")
+    print('Конец')
 
 
 async def async_select():
+    print('Начало')
     async with async_engine.connect() as con:
+        print('\nПосле подключения к контекстному менеджеру')
         query = text("SELECT * FROM text_collector")
         res = await con.execute(query)
-        res = res.all()
-        # con.commit()
+        print('\nПосле выполнения запроса')
+        res = res.fetchmany(20)
         print(f"{res=}")
+    print('\nКонец')
 
 run(async_select())
